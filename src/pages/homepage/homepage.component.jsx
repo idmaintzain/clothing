ed83927c243e7cec0be501225cf6039126e2f615
idmaintzain <iadeyinka03@gmail.com>
2022-01-25 09:25:36 +0100

    Menu Arrangements

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,6 +4,7 @@ const HomePage = () =>(
     <div className='homepage'>
         <div className='directory-menu'>
             <div className = 'menu-item'>
+            
               <div className='content'>
                 <h1 className='title'>HATS</h1>
                 <span className='subtitle'>SHOP NOW</span>
